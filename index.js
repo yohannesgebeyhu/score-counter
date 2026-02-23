39abd427479adbf24f884f1d591b44bad0477c11
yohannesgebeyhu <yohannesgebeyhu48@gmail.com>
2026-02-23 11:51:45 +0300

    commit initial files

---
+++ index.js
@@ -0,0 +1,263 @@
+// // // //document.getElementById("count").innerText =5
+
+// // // // let firstbatch = 5
+// // // // let secondbatch = 7
+// // // // let count = firstbatch + secondbatch
+
+// // // // console .log(count)
+
+// // // // let humanyears = 24
+// // // // let humanDogRario= 7
+// // // // let myDogAge = humanDogRario*humanyears
+// // // // console.log(myDogAge)
+ 
+// // // // let bonusPoint = 50
+// // // // console .log(bonusPoint)
+// // // // bonusPoint = bonusPoint + 50
+// // // // console .log(bonusPoint)
+// // // // bonusPoint = bonusPoint - 75
+// // // // console .log(bonusPoint)
+// // // // bonusPoint = bonusPoint + 45
+// // // // console .log(bonusPoint)
+// // // // function increment(){
+// // // //     console .log("The button was clicked")
+
+// // // // }
+// // // // function countdown(){
+// // // //     console .log(5)
+// // // //     console .log(4)
+// // // //     console .log(3)
+// // // //     console .log(2)
+// // // //     console .log(1)
+// // // // }
+// // // // countdown()
+// // // // countdown()
+
+// // // // function yohan(){
+// // // //     console .log(45)
+// // // // }
+// // // // yohan()
+// // // // let lap1 = 34
+// // // // let lap2 = 33
+// // // // let lap3 = 36
+
+// // // // function sum() {
+// // // //     let
+// // // //     console .log(lap1+lap2+lap3)
+// // // // }
+// // // // sum()
+
+
+// // // // let countdisplay = document.getElementById("number");
+// // // // let saveEl = document.getElementById("save-el")
+// // // // let count = 0;
+
+
+
+// // // //  function increment() {
+    
+// // // //      count += 1;
+// // // //      let formatteCount = count.toString().padStart(5, '0');
+// // // //      countdisplay.innerText = formatteCount;
+// // // // }
+// // // // console.log(count)
+// // // // function save() {
+// // // //     let countStr = count.toString().padStart(5, '0') + " - ";
+// // // //     saveEl.textContent += countStr + "  ";  
+// // // //     count = 0;
+// // // //     countdisplay.innerText = count.toString().padStart(5, '0');
+// // // // }
+// // // // function save(){
+// // // //     count = 0;
+// // // //     countdisplay.innerText = count;
+
+// // // // }
+
+// // // // let username = "joe";
+// // // // console.log(username);
+
+// // // // let message = "You have tree new notification";
+// // // // console.log(message +"  ," +username + "!");
+// // // // let messageToUser = "We have logged" +" " + message + "," + username + "!";
+// // // // console.log(messageToUser);
+
+// // // // let name = "Yohannes Gebeyhu";
+// // // // let greeting = "Hi my name is";
+// // // // let mygreeting = greeting + " " + name ;
+// // // // console.log(mygreeting);
+
+// // // // let point = 4;
+// // // // let bonusPoint ="10"
+// // // // let totalPoint = point + bonusPoint;
+// // // // console.log(totalPoint);
+// // // // console.log("5" + "1")
+// // // // console.log(100 + "1100")
+
+// // // // let wish = "i'd love to see the show \"the Lion King\"." 
+// // // // console.log(wish);
+
+// // // // let welcomeEl = document.getElementById("welcome-el");
+// // // // let name = "Yohannes Gebeyhu";
+// // // // let greeting = "Welcome to this page"
+// // // // let all = greeting + "" + name;
+// // // // welcomeEl.innerText = all  ;
+// // // // welcomeEl.innerText += "👋"
+
+// // // let incrementEl = document.getElementById("number");
+// // // let saveEl = document.getElementById("save-el");
+// // // let count = 0;
+
+// // // function increment(){
+// // //     count += 1;
+// // //     let formatteCount = count.toString().padStart(3, '2');
+// // //     incrementEl.innerText = formatteCount;
+// // // }
+// // // function save(){
+// // //     let savenol = count.toString().padStart(3, '2') + " - ";
+// // //     saveEl.textContent += savenol + " ";
+// // //     count = 0
+// // //     let fomatteNol = count.toString().padEnd(6, '7');
+// // //     incrementEl.innerText = fomatteNol;
+    
+// // // }
+// // // //  function increment() {
+    
+// // // //      count += 1;
+// // // //      let formatteCount = count.toString().padStart(5, '0');
+// // // //      countdisplay.innerText = formatteCount;
+// // // // }
+// // // // console.log(count)
+// // // // function save() {
+// // // //     let countStr = count.toString().padStart(5, '0') + " - ";
+// // // //     saveEl.textContent += countStr + "  ";  
+// // // //     count = 0;
+// // // //     countdisplay.innerText = count.toString().padStart(5, '0');
+
+// // // let firstName = "Yohannes";
+// // // let lastName = "Gebeyhu";
+// // // let fullName = firstName + " " +lastName;
+// // // console.log(fullName);
+
+// // // let name = "Linda";
+// // // let greeting = "Hi there";
+
+// // // function greetingTolinda() {
+// // //     console.log(greeting + " " + name + "!");
+// // // }
+
+// // // greetingTolinda()
+
+// // let myPoint = 3;
+
+// // function add(){
+// //     myPoint +=  3;
+// // }
+// // function min(){
+// //     myPoint -= 1;
+// // }
+// // add()
+// // add()
+// // add()
+// // min()
+// // min()
+// // console.log(myPoint);
+// // // 22
+// // // 18
+// // // 65
+// // // my points:14
+// // // 4
+// // // 1114
+// // console.log("my point:" + 5 + 9)
+
+// let errorEL = document.getElementById("error")
+// function error(){
+//     errorEL.innerText = "some thing went wrong please try agin error"
+// }
+// let num1 = 8;
+// let num2 = 2;
+// document.getElementById("num1-el").innerText = num1;
+// document.getElementById("num2-el").innerText = num2;
+// sumEl = document.getElementById("cal")
+// function sum(){
+//     first = num1 + num2;
+//     sumEl.innercontent += "sum:" + first;
+//     let savenol = first
+//     sumEl.textContent += "-" + savenol + " ";
+    
+  
+// }
+// function subtraction(){
+//     second = num1-num2;
+//     sumEl.innercontent += "sum:" + second;
+//     let savenol = second;
+//     sumEl.textContent += "-" + savenol + " ";
+// }
+// function division(){
+//     third = num1/num2;
+//     sumEl.innercontent += "sum:" + third;
+//     let savenol = third;
+//     sumEl.textContent +="-" + savenol + " ";
+// }
+// function multiplication(){
+//     forth = num1*num2;
+//     sumEl.innercontent += "sum:" + forth;
+//     let savenol = forth;
+//     sumEl.textContent +=  "-" + savenol + " " 
+// }
+
+let homeEl = document.getElementById("homescore")
+let guestEl = document.getElementById("guestscore")
+let count = 0
+let gcount = 0
+
+
+function updatedHighlight(){
+    homeEl.classList.remove("winner")
+    guestEl.classList.remove("winner")
+    if(count > gcount){
+        homeEl.classList.add("winner")
+    } 
+    else if(gcount > count){
+        guestEl.classList.add("winner")
+    }
+
+}
+function addone() { 
+    count += 1; 
+    homeEl.textContent = count; 
+    updatedHighlight() 
+}
+function addtwo() { 
+    count += 2; 
+    homeEl.textContent = count; 
+    updatedHighlight() 
+}
+function addthree() { 
+    count += 3; 
+    homeEl.textContent = count; 
+    updatedHighlight() 
+}
+function gaddone() { 
+    gcount += 1; 
+    guestEl.textContent = gcount; 
+    updatedHighlight() 
+}
+function gaddtwo() { 
+    gcount += 2; 
+    guestEl.textContent = gcount; 
+    updatedHighlight() 
+}
+function gaddthree() { 
+    gcount += 3; 
+    guestEl.textContent = gcount; 
+    updatedHighlight() 
+}
+
+function reset(){
+    count = 0
+    gcount= 0
+    homeEl.textContent = count
+    guestEl.textContent = gcount
+    homeEl.classList.remove("winner")
+    guestEl.classList.remove("winner")
+}
